bb25e5a4ea736a707af16b95dfc0fc57e2007a44
Dmitrii <34284078+matyashov1@users.noreply.github.com>
2024-03-25 09:00:31 +0300

    Create crud.py

---
+++ Seminar6/crud.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm import Session
+from . import models
+
+def create_user(db: Session, user: models.UserCreate):
+    hashed_password = "fakehashedpassword"  # Replace with actual password hashing
+    db_user = models.User(first_name=user.first_name, last_name=user.last_name, email=user.email, password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_product(db: Session, product: models.ProductCreate):
+    db_product = models.Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+
+def create_order(db: Session, order: models.OrderCreate):
+    db_order = models.Order(**order.dict())
+    db.add(db_order)
+    db.commit()
+    db.refresh(db_order)
+    return db_order
+
+
+def get_order(db: Session, order_id: int):
+    return db.query(models.Order).filter(models.Order.id == order_id).first()
